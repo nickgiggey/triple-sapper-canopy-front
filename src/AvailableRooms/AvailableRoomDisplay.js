695520a2b3a651f095d839aa781fd90b74e1cbaa
MeesumAshary <meesumashary@gmail.com>
2022-07-13 10:45:57 -0700

    Update Available Room Display

--- src/AvailableRooms/AvailableRoomDisplay.js
+++ src/AvailableRooms/AvailableRoomDisplay.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import '../RoomList/roomlist.css'
+import '../RoomList/roomlist.css';
 
 function AvailableRoomDisplay({ room }) {
+
+
+	
 	return <div className='grid-item available'>{room.roomnumber}</div>;
 }
 
