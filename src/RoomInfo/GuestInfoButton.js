d89c0465ba44b3a0fd16c55eb697cd29e2e31a5b
xelphite <stoppanij@gmail.com>
2022-07-14 09:27:04 -0700

    added guest navigate to room button

--- src/RoomInfo/GuestInfoButton.js
+++ src/RoomInfo/GuestInfoButton.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function GuestInfoButton({ room }) {
 
+	const navigate = useNavigate();
 
+	function handleClick() {
+
+		navigate(`/guestlist/${room.guestinfo._id}`);
+
+
+	}
 
 	if (!room.availability) {
-		return <button>Guest Information</button>;
+		return <button onClick={handleClick}>Guest Information</button>;
 	}
 }
 
