8b72cc78fd37cb02cae8101063b9c7ca86a494f0
xelphite <stoppanij@gmail.com>
2022-07-14 13:55:52 -0700

    added some mobile styling to home and guest form pages

--- src/Home/Body.js
+++ src/Home/Body.js
@@ -5,7 +5,7 @@ function Body() {
             <div className="pic-container">
                 <article className="pic-container-inner">
                     <header className="pic-header">Employee Portal</header>
-                    <p className="pic-explanation">Description</p>
+                    <p className="pic-explanation">Hello employee #00074b, have a happy and productive day!~</p>
                 </article>
             </div>
     );
