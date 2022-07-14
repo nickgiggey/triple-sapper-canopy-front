c669782694aab77e320f656a54082336d8855c10
xelphite <stoppanij@gmail.com>
2022-07-14 11:49:51 -0700

    added room filtering on assignment by partysize, added roominfo check on unsubscribe guest.
    Co-authored-by: bdnkai <52808679+bdnkai@users.noreply.github.com>

--- src/AvailableRooms/AvailableRoomList.js
+++ src/AvailableRooms/AvailableRoomList.js
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 function AvailableRoomList(props) {
 	const [rooms, setRooms] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [guestInfo, setGuestInfo] =useState({})
 	const { guestId } = useParams();
 
 	async function getRooms() {
@@ -13,10 +14,16 @@ function AvailableRoomList(props) {
 			const response = await axios.get(
 				'https://secret-waters-54413.herokuapp.com/api/rooms'
 			);
-			const results = await response.data.filter(
-				(result) => result.availability
-			);
-			setRooms(results);
+			setRooms(response.data);
+		} catch (error) {
+			setLoading(false);
+		}
+	}
+
+	async function getGuestInfo(id) {
+		try {
+			const guest = await axios.get(`https://secret-waters-54413.herokuapp.com/api/guests/${id}`);
+			setGuestInfo(guest.data);
 		} catch (error) {
 			setLoading(false);
 		}
@@ -28,7 +35,7 @@ function AvailableRoomList(props) {
 				setLoading(false);
 			}
 		}, 5000);
-
+		getGuestInfo(guestId);
 		// Write your GET fetch() or axios() request here
 		getRooms();
 
@@ -47,7 +54,11 @@ function AvailableRoomList(props) {
 		<div className='home-container2'>
 			<h1 className='header'>Room Availability</h1>
 			<div className='grid-container'>
-				{rooms.map((room, index) => {
+				{rooms
+					.filter((room) => {
+						return (room.availability && room.roomsize >= guestInfo.partySize)
+					})
+					.map((room) => {
 					return (
 						<AvailableRoomDisplay
 							room={room}
